e3886064b8bb5e676a1ccdd556b299057feb297a
YoAhdi <ahadahmad.work@gmail.com>
2024-08-16 01:35:15 -0700

    New changes 44:00

--- app/page.js
+++ app/page.js
@@ -32,8 +32,10 @@ export default function Home() {
           <Box sx={{
             textAlign: 'center', my: 4,
           }}>
-            <Typography variant="h2">Welcome to Flashcard SaaS</Typography>
-            <Typography variant="h5"> 
+            <Typography variant="h2" gutterBottom>
+              Welcome to Flashcard SaaS
+              </Typography>
+            <Typography variant="h5" gutterBottom> 
               {' '}
               The Easiest way to make flashcards from your text
               </Typography>
@@ -43,28 +45,78 @@ export default function Home() {
           </Box>
 
           <Box sx = {{my: 6}}>
-            <Typography varaint="h4" components= "h2">
-              Features
-            </Typography>
-            <Grid contained spacing = {4}>
-              <Grid item xs={12} md={4}></Grid>
-              <Typography variant="h6">Easy Text Input</Typography>
+            <Typography varaint="h4" gutterBottom>Features</Typography>
+            <Grid container spacing = {4}>
+              <Grid item xs={12} md={4}>
+              <Typography variant="h6" gutterBottom>Easy Text Input</Typography>
               <Typography> 
                 {' '}
-                Simply input your text and let our software do the rest. Creating flashcards has never been easier.</Typography>
+                Simply input your text and let our software do the rest. Creating flashcards has never been easier.
+                </Typography>
             </Grid>
-            <Grid item xs={12} md={4}></Grid>
-              <Typography variant="h6">Smart Flashcards</Typography>
+            <Grid item xs={12} md={4}>
+              <Typography variant="h6" gutterBottom>Smart Flashcards</Typography>
               <Typography> 
                 {' '}
                 Our AI inteligently breaks down your text into concise flashcards, perfect for studying.</Typography>
-              <Grid item xs={12} md={4}></Grid>
-              <Typography variant="h6">Accessible anywhere</Typography>
+                </Grid>
+              <Grid item xs={12} md={4}>
+              <Typography variant="h6" gutterBottom>Accessible anywhere</Typography>
               <Typography> 
                 {' '}
                 Access your flashcards from any device at any time. Study on the go with ease</Typography>
-          </Box>
+          </Grid>
+        </Grid>
+        </Box>
 
+          <Box sx={{my:6, textAlign: 'center'}}>
+            <Typography varaint="h4" gutterBottom>Pricing</Typography>
+            <Grid container spacing = {4}>
+              <Grid item xs={12} md={6}>
+              <Box sx={{
+                p: 3,
+                border: '1px solid',
+                borderColor: 'grey.300',
+                borderRadius: 2,
+                }}
+                >
+              <Typography variant="h5" gutterBottom>Basic</Typography>
+              <Typography variant="h6" gutterBottom>$5 / month</Typography>
+              <Typography> 
+                {' '}
+                  Access to basic flashcard features and limited storage. 
+                </Typography>
+                <Button variant = "contained" color = "primary" sx={{mt: 2}}> 
+                  Choose basic
+                </Button>
+                </Box>
+            </Grid>
+          
+            
+            <Grid item xs={12} md={6}>
+          <Box sx={{
+                p: 3,
+                border: '1px solid',
+                borderColor: 'grey.300',
+                borderRadius: 2,
+                }}
+                >
+              <Typography variant="h5" gutterBottom>Pro</Typography>
+              <Typography variant="h6" gutterBottom>$10 / month</Typography>
+              <Typography> 
+                {' '}
+                  Unlimited flashcard and storage, and priority support.
+                </Typography>
+                <Button variant = "contained" color = "primary" sx={{mt: 2}}> 
+                  Choose basic
+                </Button>
+                </Box>
+                </Grid>
+          
+
+
+        </Grid>
+          </Box>
 
         </Container>
       </CustomSWRConfig>
